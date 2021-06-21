e1bce78a02863c3346010d0a0350824bf344f5de
Mary Beth <mbingramm@gmail.com>
2021-06-21 17:23:59 -0600

    useEffect

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import Premade4 from './Premade4'
 import Divider1 from './Divider1'
 import Divider2 from './Divider2'
 import Divider3 from './Divider3'
-// import Loading from './Loading'
-// import Error from './Error'
+import Loading from './Loading'
+import Error from './Error'
 
 const baseURL = "http://colormind.io/api/"
 const paletteSeeds = "https://swatch-swap-default-rtdb.firebaseio.com/palettes.json"
@@ -23,62 +23,44 @@ export default function App() {
   const [premadeTwo, setPaletteTwo] = useState([])
   const [premadeThree, setPaletteThree] = useState([])
   const [premadeFour, setPaletteFour] = useState([])
-  // const [isLoading, setIsLoading] = useState(false)
-  // const [errorMessage, setErrorMessage] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState("")
 
-  // const timeoutHandler = (time, promise) => {
-  //   return new Promise((resolve, reject) => {
-  //     const timer = setTimeout(() => {
-  //       reject(new Error("API Offline"))
-  //     }, time)
-  //     promise
-  //       .then(value => {
-  //         clearTimeout(timer)
-  //         resolve(value)
-  //       })
-  //       .catch(reason => {
-  //         clearTimeout(timer)
-  //         reject(reason)
-  //       })
-  //   })
-  // }
-
-  // const loadColors = () => {
-  //   console.log("loadColors")
-  //   setIsLoading(true)
-  //   timeoutHandler(10000, fetch(baseURL, {
-  //     method: 'POST',
-  //     body: JSON.stringify({
-  //       model: 'default',
-  //     })
-  //   }))
-  //     .then(response => {
-  //       console.log("response", response)
-  //       return response.json()
-  //     })
-  //     .then(colors => {
-  //       console.log("colors", colors)
-  //       setIsLoading(false)
-  //       setColors(colors.result)
-  //     })
-  //     .catch(error => {
-  //       setErrorMessage("API Offline")
-  //       setIsLoading(false)
-  //     })
-  // };
+  const timeoutHandler = (time, promise) => {
+    return new Promise((resolve, reject) => {
+      const timer = setTimeout(() => {
+        reject(new Error("API Offline"))
+      }, time)
+      promise
+        .then(value => {
+          clearTimeout(timer)
+          resolve(value)
+        })
+        .catch(reason => {
+          clearTimeout(timer)
+          reject(reason)
+        })
+    })
+  }
 
   const loadColors = () => {
-    fetch(baseURL, {
+    setIsLoading(true)
+    timeoutHandler(10000, fetch(baseURL, {
       method: 'POST',
       body: JSON.stringify({
         model: 'default',
       })
-    })
-    .then(response => response.json())
-    .then(colors => {
-      setColors(colors.result)
-    })
-  }
+    }))
+      .then(response => response.json())
+      .then(colors => {
+        setIsLoading(false)
+        setColors(colors.result)
+      })
+      .catch(error => {
+        setErrorMessage("API Offline")
+        setIsLoading(false)
+      })
+  };
 
 
   const loadPalettes = () => {
@@ -92,9 +74,24 @@ export default function App() {
       })}
 
   useEffect ( () => {
-    loadColors()
+    setIsLoading(true)
+    timeoutHandler(10000, fetch(baseURL, {
+      method: 'POST',
+      body: JSON.stringify({
+        model: 'default',
+      })
+    }))
+      .then(response => response.json())
+      .then(colors => {
+        setIsLoading(false)
+        setColors(colors.result)
+      })
+      .catch(error => {
+        setErrorMessage("API Offline")
+        setIsLoading(false)
+      })
     loadPalettes()
-  } )
+  }, [] )
 
     return (
       <div className="App">
@@ -102,8 +99,8 @@ export default function App() {
         <NavBar
           loadColors={loadColors}
           colors={colors}/>
-        {/* {isLoading ? <Loading message="Loading API" /> : null }
-        {errorMessage ? <Error message={errorMessage} /> : null } */}
+        {isLoading ? <Loading message="Loading API" /> : null }
+        {errorMessage ? <Error message={errorMessage} /> : null }
         <ColorsDiv
           colors={colors} />
         <Divider1 />
