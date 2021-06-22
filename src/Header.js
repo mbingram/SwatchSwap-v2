0b333699ec4b913aa7f4e33b1bf908012db0e6af
Mary Beth <mbingramm@gmail.com>
2021-06-22 10:24:30 -0600

    useCallback fixes infinite loop

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,21 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+import React from "react";
 import logo from './swatch_logo.png'
-import { ChromePicker } from 'react-color';
+// import { ChromePicker } from 'react-color';
 
 export default function Header() {
 
-    const [background, setBackground] = useState([])
+    // const [background, setBackground] = useState("#fff")
 
-    const handleChangeComplete = (color) => {
-        setBackground({ background: color.hex });
-    };
+    // const handleChangeComplete = (color) => {
+    //     setBackground({ background: color.hex });
+    // };
 
         return (
             <div className="header">
-                <ChromePicker className="chrome-picker"
+                {/* <ChromePicker className="chrome-picker"
                     color={ background }
-                    onChangeComplete={ handleChangeComplete } />
+                    onChangeComplete={ handleChangeComplete } /> */}
                 <img src={logo} alt="logo" id="logo" className="logo"></img>
                 <h5 className="header-links"><a href="http://colormind.io/image/">Get a Pallete From an Image</a> | <a href="http://colormind.io/api-access/">Hit the API</a></h5>
             </div>
